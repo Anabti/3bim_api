4d18d1fe5dae15fd2a62ff9b8f5f28632e9a6bb9
Anabti <anabeatrizzt.i@gmail.com>
2026-08-26 11:45:05 -0300

    exercicio- create livro

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from sqlalchemy.orm import Session
 from database import Base, engine, get_db
 from models import ProdutoDB
 from schemas import ProdutoCreate, ProdutoResponse
+from models import LivroDB
+from schemas import LivroCreate, LivroResponse
+from fastapi import HTTPException
+
+
 
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -59,3 +64,51 @@ def atualizar_produto(produto_id: int, dados: ProdutoCreate, db:
         db.commit()
         db.refresh(produto)
         return produto
+
+#Livros
+
+# GET /livro/{id} -> retorna um único livro pelo id
+@app.get('/livro /{livro _id}', response_model=LivroResponse)
+def obter_livro (livro_id: int, db: Session = Depends(get_db)):
+    livro  = db.query(LivroDB).filter(LivroDB.id == livro_id).first()
+    if livro  is None:
+        raise HTTPException(status_code=404, detail='Livro não encontrado')
+        return livro 
+
+# DELETE /livro /{id} -> remove um livro  do banco de dados
+@app.delete('/livro /{produto_id}', status_code=204)
+def remover_livro (produto_id: int, db: Session = Depends(get_db)):
+    livro = db.query(LivroivroDB).filter(LivroDB.id == livro_id).first()
+    if livro  is None:
+        raise HTTPException(status_code=404, detail='Livro  não encontrado')
+    db.delete(livro)
+    db.commit()
+
+@app.get('/livro ', response_model=list[LivroResponse])
+def listar_livro (db: Session = Depends(get_db)):
+    return db.query(Livro DB).all()
+    
+@app.post('/livro ', response_model=LivroResponse, status_code=201)
+def criar_livro (livro: LivroCreate, db: Session = Depends(get_db)):
+    novo_livro  = LivroDB(**livro.dict())
+    db.add(novo_livro )
+    db.commit()
+    db.refresh(novo_livro )
+    return novo_livro 
+
+# PUT /livro/{id} -> atualiza um livro existente no banco
+@app.put('/livro/{livro_id}', response_model=LivroResponse)
+def atualizar_livro(livro_id: int, dados: LivroCreate, db:
+    Session = Depends(get_db)):
+        livro = db.query(LivroDB).filter(LivroDB.id == livro_id).first()
+        if livro is None:
+            raise HTTPException(status_code=404, detail='Livro não encontrado')
+        livro.titulo = dados.titulo
+        livro.autor = dados.autor
+        livro.ano_publicacao = dados.ano_publicacao
+        livro.quantidade = dados.quantidade
+        livro.preco = dados.preco
+        db.commit()
+        db.refresh(livro)
+        return livro
+
--- schemas.py
+++ schemas.py
@@ -13,3 +13,19 @@ class ProdutoResponse(ProdutoBase):
 
 class Config:
     from_attributes = True 
+
+class LivroBase(BaseModel):
+    titulo: str
+    autor: str
+    ano_publicaco: int
+    preco: float
+    quantidade: int
+
+class LivroCreate(LivroBase):
+    pass
+
+class LivroResponse(LivroBase):
+    id: int
+
+class Config:
+    from_attributes = True 
